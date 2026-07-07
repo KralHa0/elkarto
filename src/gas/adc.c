5392418a2128405f01902f43edb4d4ec90f3b80f
Cetin <cetin.harun03@gmail.com>
2026-07-07 11:38:25 +0200

    Gas pot and servo with updated pinout.

--- src/gas/adc.c
+++ src/gas/adc.c
@@ -4,20 +4,20 @@ ADC_HandleTypeDef hadc;
 
 void adc1Init(void) {
     __HAL_RCC_ADC1_CLK_ENABLE();
-    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOC_CLK_ENABLE();
 
     GPIO_InitTypeDef gpio = {0};
-    gpio.Pin = GPIO_PIN_3;
+    gpio.Pin = GPIO_PIN_0;
     gpio.Mode = GPIO_MODE_ANALOG;
     gpio.Pull = GPIO_NOPULL;
-    HAL_GPIO_Init(GPIOA, &gpio);
+    HAL_GPIO_Init(GPIOC, &gpio);
 
     hadc.Instance = ADC1;
     hadc.Init.Resolution = ADC_RESOLUTION_12B;
     HAL_ADC_Init(&hadc);
 
     ADC_ChannelConfTypeDef channel = {0};
-    channel.Channel = ADC_CHANNEL_3;
+    channel.Channel = ADC_CHANNEL_10;
     channel.Rank = 1;
     channel.SamplingTime = ADC_SAMPLETIME_84CYCLES;
     HAL_ADC_ConfigChannel(&hadc, &channel);
